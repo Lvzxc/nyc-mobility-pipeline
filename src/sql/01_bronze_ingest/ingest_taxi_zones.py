6a2d353f9d0af53cb68e6a55714d1f64468b5b3d
triciakieth <triciakiethtaneo@gmail.com>
2026-09-16 20:58:27 +0800

    fix: add ingestion timestamps and dates

--- src/sql/01_bronze_ingest/ingest_taxi_zones.py
+++ src/sql/01_bronze_ingest/ingest_taxi_zones.py
@@ -6,7 +6,7 @@ from pyspark.sql import functions as F
 source_path = "/Volumes/nyc/default/nyc-mobility-volume/taxi_zones/taxi_zone_lookup.csv"
 
 # Bronze table
-bronze_table = "nyc.nyc_bronze.taxi_zone_lookup"
+bronze_table = "nyc.nyc_bronze.taxi_zone_bronze"
 
 # Read CSV
 df = (
@@ -16,17 +16,25 @@ df = (
     .csv(source_path)
 )
 
+# Add ingestion metadata
+df = (
+    df
+    .withColumn("ingestion_timestamp", F.current_timestamp())
+    .withColumn("ingestion_date", F.current_date())
+)
+
 # Show schema
 df.printSchema()
 
 # Preview data
-display(df)
+display(df.limit(20))
 
 # Write to Bronze as Delta
 (
     df.write
     .format("delta")
     .mode("overwrite")
+    .option("overwriteSchema", "true")
     .saveAsTable(bronze_table)
 )
 
