e578d0d4b53b3426886e561899c037dea1c2588d
sara.celadina@ftwfoundation.org <sara.celadina@ftwfoundation.org>
2026-09-18 11:31:36 +0000

    added taxi_demand analytics
    Business Question: When and where is the taxi demand highest

--- src/sql/05_analytics/taxi_demand.sql
+++ src/sql/05_analytics/taxi_demand.sql
@@ -1,1 +1,32 @@
+-- When is taxi demand highest?
+-- Aggregates taxi trips by day of the week and hour.
+SELECT 
+    d.day_name,
+    d.hour,
+    COUNT(f.trip_key) AS trip_count
+FROM nyc.nyc_gold.fact_taxi_trip f
+JOIN nyc.nyc_gold.dim_datetime d 
+    ON f.pickup_datetime_key = d.datetime_key
+GROUP BY 
+    d.day_name,
+    d.hour,
+    d.day_of_week
+ORDER BY 
+    d.day_of_week,
+    d.hour;
 
+
+-- Where is taxi demand highest?
+-- Aggregates taxi trips by pickup zone and borough
+SELECT 
+    l.zone,
+    l.borough,
+    COUNT(*) AS trip_count
+FROM nyc.nyc_gold.fact_taxi_trip f
+JOIN nyc.nyc_gold.dim_location l 
+    ON f.pickup_location_key = l.location_key
+GROUP BY 
+    l.zone, 
+    l.borough
+ORDER BY 
+    trip_count DESC;
